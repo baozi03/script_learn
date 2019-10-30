6a1aaced7c593eda477144357e82577b7829806c
baizi03 <1538252947@qq.com>
2019-10-30 20:55:29 +0800

    类方法、实例方法、静态方法

--- learn03.py
+++ learn03.py
@@ -1,48 +1,22 @@
-#购买一栋房子，把家具放进去，打印出面积
-#创建房子的类
-class Home:
-    def __init__(self,new_area,new_info,new_address):
-        self.area = new_area
-        self.info = new_info
-        self.address = new_address
-        self.left_area = new_area
-        self.contact_item = []
-    
-    def __str__(self):
-       msg = "房子的面积是：%d,剩余面积是：%d,户型是：%s,地址是：%s"%(self.area,self.left_area,self.info,self.address)
-       msg += "当前房子的家具有%s"%(str(self.contact_item))
-       return msg
+class Game():
+    #类属性
+    num = 0
+    def __init__(self):
+        #实例属性
+        self.name ="laowang"
+    #类方法
+    @classmethod
+    def add_num(cls):
+        cls.num = 100
+    #静态方法
+    @staticmethod
+    def print_menu():
+        print("=============")
+        print("穿越火线")
+        print("============")
 
-    def add_item(self,item):
-        self.contact_item.append(item.get_name())
-        self.left_area -=item.get_area()
-        #self.contact_item.append(item.name)
-        #self.left_area -=item.area
-    def __del__(self):
-        print("...结束对象，释放空间...")
-
-
-#创建家具床的类
-class Bed:
-    def __init__(self,new_name,new_area):
-        self.name = new_name
-        self.area = new_area
-    
-    def __str__(self):
-        return "%s面积是%d平方"%(self.name,self.area)
-    def get_area(self):
-        return self.area
-    def get_name(self):
-        return self.name
-
-
-#创建对象
-jia = Home(120,"三房一厅","北京市 朝阳区 长安街 666号")
-print(jia)
-bed_1 = Bed("席梦思",4)
-print(bed_1)
-jia.add_item(bed_1)#这里调用的对象指向home的函数add_item的参数里
-print(jia)
-bed_2 = Bed("三人床",6)
-jia.add_item(bed_2)
-print(jia)
+game = Game()
+Game.add_num()#改写类属性 
+print(Game.num)
+print(Game.print_menu())#通过类去调用静态方法
+print(game.print_menu())#通过实例对象调用静态方法
